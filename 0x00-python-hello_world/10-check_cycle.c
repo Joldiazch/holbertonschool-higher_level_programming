1a3bd97e51a3c031dba097722ebe99065ca7dcf0
Jose <jluis.diaz@udea.edu.co>
2019-12-09 22:52:05 +0000

    push for chage of machine

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - prints all elements of a listint_t list
+ * @list: pointer to head of list
+ * Return: 1 if is a cycle list o 0 in other case.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *temp_lie, *tem_tor;
+}
