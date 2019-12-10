c9d6a1fad6a915709d892bdcdf68687bef07b83e
Holberton <holberton@Holbertons-iMac-45.local>
2019-12-09 19:41:48 -0500

    no recursion only in this case use while

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,21 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-/**
- * cycle - prints all elements of a listint_t list
- * @tort: pointer to head of list
- * @lieb: pinter to tow step
- * Return: 1 if is a cycle list o 0 in other case.
- */
-int cycle(listint_t *tort, listint_t *lieb)
-{
-	if (lieb == tort)
-		return (1);
-	else if (lieb == NULL || tort == NULL)
-		return (0);
-	else
-		return (cycle(tort->next, lieb->next->next));
-}
+
 /**
  * check_cycle - prints all elements of a listint_t list
  * @list: pointer to head of list
@@ -23,5 +9,19 @@ int cycle(listint_t *tort, listint_t *lieb)
  */
 int check_cycle(listint_t *list)
 {
-	return (cycle(list, list->next->next));
+	listint_t *tort, *lieb;
+
+	tort = list;
+	lieb = list;
+	if (list)
+	{
+		while (tort && tort->next && lieb && lieb->next)
+		{
+			tort = tort->next;
+			lieb = lieb->next->next;
+			if (lieb == tort)
+				return (1);
+		}
+	}
+	return (0);
 }
