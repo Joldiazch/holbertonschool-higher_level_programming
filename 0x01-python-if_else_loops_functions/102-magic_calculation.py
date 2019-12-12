a00bc5838f1c13d9968a3dcef3467e1afb3090ed
Jose <jluis.diaz@udea.edu.co>
2019-12-12 00:23:58 +0000

    ByteCode -> Python #2

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
-    if(a < b):
+    if a < b:
         return (c)
-    if(c > b):
-        return (a + b)
-    return((a * b) - b)
+    if c > b:
+        return a + b
+    return a * b - b
