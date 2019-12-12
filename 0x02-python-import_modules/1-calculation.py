b96a26830be056eac0c613986270a059774fa781
Jose <jluis.diaz@udea.edu.co>
2019-12-12 17:53:58 +0000

    Import a simple function from a simple file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    a, b = 10, 5
-    result_sum = add(a, b)
-    print('{:d} + {:d} = {:d}'.format(a, b, result_sum))
-    result_res = sub(a, b)
-    print('{:d} - {:d} = {:d}'.format(a, b, result_res))
-    result_mul = mul(a, b)
-    print('{:d} * {:d} = {:d}'.format(a, b, result_mul))
-    result_div = div(a, b)
-    print('{:d} / {:d} = {:d}'.format(a, b, result_div))
+    a = 10
+    b = 5
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+    print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+    print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+    print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
