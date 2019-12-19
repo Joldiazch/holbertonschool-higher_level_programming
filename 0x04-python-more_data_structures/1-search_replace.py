92c40eaef3335eaf7edeea535ab2040494706922
Jose <jluis.diaz@udea.edu.co>
2019-12-19 18:04:05 +0000

    replaces all occurrences of an element by another in a new list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     if my_list:
-        lista = list(map(lambda x: replace if x == search else x, my_list))
-    return lista
+        return list(map(lambda x: replace if x == search else x, my_list))
