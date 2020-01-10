aa5789ea32b7b66b64787a137b2624de9905009f
joldiazch <jluis.diaz@udea.edu.co>
2020-01-10 03:48:59 +0000

    class Square that defines a square by: (based on 5-square.py)

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 class Square:
-
-
     def __init__(self, size_of_square=0, position=(0, 0)):
         if type(size_of_square) is not int:
             raise TypeError('size must be an integer')
@@ -13,17 +11,20 @@ class Square:
             raise TypeError('position must be a tuple of 2 positive integers')
         elif len(position) != 2:
             raise TypeError('position must be a tuple of 2 positive integers')
-        elif (type(position[0]) != int) or (type(position[1]) != int):
-            raise TypeError('position must be a tuple of 2 positive integers')	
+        elif (type(position[0]) != int) and (type(position[1]) != int):
+            raise TypeError('position must be a tuple of 2 positive integers')
         elif (position[0] < 0) or (position[1] < 0):
             raise TypeError('position must be a tuple of 2 positive integers')
         else:
             self.__position = position
+
     def area(self):
         return self.__size ** 2
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if type(value) is not int:
@@ -32,22 +33,25 @@ class Square:
             raise ValueError('size must be >= 0')
         else:
             self.__size = value
+
     @property
     def position(self):
         return self.__position
+
     @position.setter
     def position(self, value):
         if type(value) is not tuple:
             raise TypeError('position must be a tuple of 2 positive integers')
         elif len(value) != 2:
             raise TypeError('position must be a tuple of 2 positive integers')
-        elif (type(value[0]) != int) or (type(value[1]) != int):
-            raise TypeError('position must be a tuple of 2 positive integers')	
+        elif (type(value[0]) != int) and (type(value[1]) != int):
+            raise TypeError('position must be a tuple of 2 positive integers')
         elif (position[0] < 0) or (position[1] < 0):
             raise TypeError('position must be a tuple of 2 positive integers')
         else:
             self.__position = value
         pass
+
     def my_print(self):
         if self.size == 0:
             print()
