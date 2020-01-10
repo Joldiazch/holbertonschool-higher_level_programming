4f6965e92e0c781b2aeebf287f1bf691260d849c
joldiazch <jluis.diaz@udea.edu.co>
2020-01-10 04:17:08 +0000

    class Square that defines a square by: (based on 5-square.py)

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -54,7 +54,7 @@ class Square:
     def my_print(self):
         if self.size == 0:
             print()
-	else:
+        else:
             print("\n" * self.position[1], end="")
         for i in range(self.size):
             print(" " * self.position[0], end="")
