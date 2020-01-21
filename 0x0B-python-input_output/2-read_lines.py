37afeca0897bba55cd5da543654f1e80d83e07dc
Holberton <holberton@Holbertons-iMac-45.local>
2020-01-21 17:37:11 -0500

    reads n lines of a text file (UTF8) and prints it to stdout

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -5,7 +5,7 @@ def read_lines(filename="", nb_lines=0):
     with open(filename, encoding="utf-8") as f:
         if nb_lines <= 0 or nb_lines >= len(f.readlines()):
             f.seek(0)
-            print(f.read())
+            print(f.read(), end="")
         else:
             for idx in range(nb_lines):
                 f.seek(0)
