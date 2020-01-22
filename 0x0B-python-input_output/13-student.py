fe90a8ee47f62d9028ee3a05f575d910936f31fe
Holberton <holberton@Holbertons-iMac-45.local>
2020-01-21 19:26:14 -0500

    Student that defines a student

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list:
+            if all(map(lambda x: True if type(x) is str else False, attrs)):
+                my_dict = {}
+                for attr in attrs:
+                    try:
+                        my_dict[attr] = getattr(self, attr)
+                    except:
+                        continue
+                return my_dict
+            else:
+                return self.__dict__
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
