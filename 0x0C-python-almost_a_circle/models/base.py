74c2092ab51f0fd01e71524a8cab90d6a9ad886e
Jose Luis diaz <jluis.diaz@udea.edu.co>
2020-01-27 07:08:26 -0500

    19 no Done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,7 +21,38 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
+        file_name = cls.__name__ + ".json"
+        list_dict = []
         if list_objs is not None:
-            my_json = cls.to_json_string(list_objs)
-            with open("Rectangle.json", mode="w") as f:
-               f.write(my_json)
+            for obj in list_objs:
+                list_dict.append(obj.to_dictionary())
+        my_json = cls.to_json_string(list_dict)
+        with open(file_name, mode="w") as f:
+            f.write(my_json)
+
+    def from_json_string(json_string):
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        dummy = cls(10,10)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        file_name = cls.__name__ + ".json"
+        try:
+            with open(file_name, mode="r") as f:
+                list_dict = cls.from_json_string(f.read())
+            list_instances = []
+            for dictionary in list_dict:
+                list_instances.append(cls.create(dictionary))
+            print(list_instances)
+            return list_instances
+        except:
+            return []
+
+        
