a38d76fae0074a8dacf0ae182d02248f0590871d
joldiazch <jluis.diaz@udea.edu.co>
2020-03-28 21:39:24 +0000

    Cities in state

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+from sys import argv
+
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+class City(Base):
+    """ Class representation for cities table """
+
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, autoincrement=True, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), unique=True)
