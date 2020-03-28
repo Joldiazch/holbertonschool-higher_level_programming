c722c59d747f3314fbfa4357a70dbe3c6b7a8313
joldiazch <jluis.diaz@udea.edu.co>
2020-03-28 18:13:22 +0000

    update task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,13 +16,12 @@ if __name__ == '__main__':
         cur.execute("""
         SELECT *
         FROM states
-        WHERE name LIKE BINARY 'N%'
+        WHERE name REGEXP BINARY '^(N)'
         ORDER BY id ASC
         """)
         query_rows = cur.fetchall()
 
         for row in query_rows:
-            if row
             print(row)
         cur.close()
         db.close()
