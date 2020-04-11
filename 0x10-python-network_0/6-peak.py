4c238370dad1c46025695d3d515838daaa15bdbd
joldiazch <jluis.diaz@udea.edu.co>
2020-04-11 01:35:07 +0000

    aplicate quick sort algoritm and return the max

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,13 +1,30 @@
 #!/usr/bin/python3
 
 
-def find_peak(list_of_integers):
-	""" return the pick of an lis of integer """
+def sort_list(lis, start, end):
+    """ sort a llist """
+    if (end - start) < 1:
+        return True
 
-	if len(list_of_integers) > 0:
-		peacks = []
-		integers = list_of_integers.copy()
-		for idx, num in enumerate(integers[1:-1]):
-			if integers[idx - 1] <= num and num >= integers[idx + 1]:
-				peacks.append(num)
-		return max(peacks)
+    pivot = lis[end]
+    i = start
+    for idx in range(start, end + 1):
+        if lis[idx] < pivot:
+            lis[i], lis[idx] = lis[idx], lis[i]
+            i += 1
+
+    if pivot < lis[i]:
+        lis[i], lis[end] = lis[end], lis[i]
+
+    sort_list(lis, start, i - 1)
+    sort_list(lis, i + 1, end)
+
+
+def find_peak(list_of_integers):
+    """ return the pick of an lis of integer """
+    size = len(list_of_integers)
+    if size > 0:
+        sort_list(list_of_integers, 0, size - 1)
+        return list_of_integers[-1]
+    else:
+        return None
