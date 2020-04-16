f4dd1375a1360156021ea00c7bae8b91b425a83c
joldiazch <jluis.diaz@udea.edu.co>
2020-04-16 03:52:56 +0000

    displays the body of the response.

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" script that fetches https://intranet.hbtn.io/status """
+import requests
+from sys import argv as args
+
+
+if __name__ == "__main__":
+    URL = args[1]
+    r = requests.post(url=URL)
+    if r.status_code < 400:
+        print(r.text)
+    else:
+        print('Error code: {}'.format(r.status_code))
