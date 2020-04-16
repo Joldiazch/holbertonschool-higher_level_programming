99497a9fc50b8ca3f3d8d63594dda082d155197d
joldiazch <jluis.diaz@udea.edu.co>
2020-04-16 03:12:28 +0000

    get variable X-Request-Id in the response header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" script that fetches https://intranet.hbtn.io/status """
+import requests
+from sys import argv as args
+
+
+if __name__ == "__main__":
+    r = requests.get(args[1])
+    print(r.headers['x-request-id'])
