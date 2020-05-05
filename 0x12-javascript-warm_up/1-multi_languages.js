14018355bccb2b03a26d2c2617aa28f497e4a298
joldiazch <jluis.diaz@udea.edu.co>
2020-05-05 17:21:31 +0000

    print 3 msg console.log

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+/* script that prints 3 lines */
+const myVarC = 'C is fun';
+const myVarP = 'Python is cool';
+const myVarJ = 'Javascript is amazing';
+console.log(myVarC);
+console.log(myVarP);
+console.log(myVarJ);
