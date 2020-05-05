bc2fecf33f206ab619f1f6550ff3bbbeb1e58771
joldiazch <jluis.diaz@udea.edu.co>
2020-05-05 17:22:48 +0000

    print message if arguments passed is >= 1

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+/* script that prints a message
+depending of the number of arguments passed */
+
+const myArgs = process.argv.slice(2);
+
+console.log(myArgs.length >= 1 ? 'Argument found' : 'No argument');
