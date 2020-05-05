8f654d8bd6eef0a55662d9eb1e4cd02f00384b51
joldiazch <jluis.diaz@udea.edu.co>
2020-05-05 18:31:37 +0000

    print a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+/* script that prints a square */
+const myArgs = process.argv.slice(2);
+const size = parseInt(myArgs[0], 10);
+
+if (size) {
+  for (let i = 0; i < size; i++) {
+    console.log('x'.repeat(size))
+  }
+} else {
+  console.log('Missing size')
+}
