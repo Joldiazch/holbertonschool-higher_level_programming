2d8f7e267bfbb539e6599925f05abfebda018ad3
joldiazch <jluis.diaz@udea.edu.co>
2020-05-05 17:36:25 +0000

    for like C

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/* script that prints 3 lines:
+(like 1-multi_languages.js) but by
+using an array of string and a loop */
+
+const msgs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let i = 0; i < msgs.length; i++) {
+	console.log(msgs[i])
+}
