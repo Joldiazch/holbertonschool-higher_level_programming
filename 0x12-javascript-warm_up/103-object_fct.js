401c488a4fb4bbd7ad72df4790f9b47502707e5b
joldiazch <jluis.diaz@udea.edu.co>
2020-05-05 22:03:04 +0000

    create method of a object

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -5,8 +5,8 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function incr () {
-  myObject.value += 1;
+myObject.incr = function () {
+  this.value += 1;
 };
 myObject.incr();
 console.log(myObject);
