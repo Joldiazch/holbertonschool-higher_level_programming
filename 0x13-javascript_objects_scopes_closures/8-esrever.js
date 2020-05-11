b122e6d5a417e069894ae1ed3e35c1551c470ab7
joldiazch <jluis.diaz@udea.edu.co>
2020-05-11 17:07:14 +0000

    odificate var variable

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,7 +3,7 @@
 /* function that returns the reversed version of a list */
 
 exports.esrever = function (list) {
-  var litsOut = [];
+  let litsOut = [];
   for (let i = list.length - 1; i >= 0; i--) {
     litsOut.push(list[i]);
   }
