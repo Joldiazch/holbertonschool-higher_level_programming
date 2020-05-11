11a53b3ebba8eabb7f4e8e9401a9d1d91a6eed8c
Joldiazch <54413604+Joldiazch@users.noreply.github.com>
2020-05-11 12:58:39 -0500

    update

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -2,7 +2,7 @@
 
 /* function that prints the number of
 arguments already printed and the new argument value */
-var numArgPrinted = 0;
+let numArgPrinted = 0;
 exports.logMe = function (item) {
   console.log(numArgPrinted + ': ' + item);
   numArgPrinted++;
