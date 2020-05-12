62b21b798feaea75d2c437d38bba068c9e320b7e
joldiazch <jluis.diaz@udea.edu.co>
2020-05-12 19:36:03 +0000

    computes the number of tasks completed by user id in api

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+/* script that prints the title of a Star Wars movie
+where the episode number matches a given integer. */
+
+const request = require('request');
+
+const myArgs = process.argv.slice(2);
+const apiUrl = myArgs[0];
+request(
+  apiUrl,
+  function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      const allObjects = JSON.parse(body);
+      const info = {};
+      allObjects.forEach(obj => {
+        if (info[obj.userId]) {
+          obj.completed ? info[obj.userId] += 1 : info[obj.userId] += 0;
+        } else {
+          obj.completed ? info[obj.userId] = 1 : info[obj.userId] = 0;
+        }
+      });
+      console.log(info);
+    }
+  });
